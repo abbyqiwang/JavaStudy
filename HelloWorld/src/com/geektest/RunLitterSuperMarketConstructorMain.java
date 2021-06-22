21f3f47646a12b6a5a929f35b239d54959b76c6a
abbyqiwang <abbyqiwang@webank.com>
2021-06-22 23:42:27 +0800

    20210619V1.0 构造函数再应用

--- HelloWorld/src/com/geektest/RunLitterSuperMarketConstructorMain.java
+++ HelloWorld/src/com/geektest/RunLitterSuperMarketConstructorMain.java
@@ -1,10 +1,7 @@
 package com.geektest;
 
 import com.geektest.supermarket.LitterSuperMarketConstructor;
-import com.geektest.supermarket.Merchandise;
 import com.geektest.supermarket.MerchandiseConstructor;
-
-import java.util.ArrayList;
 
 public class RunLitterSuperMarketConstructorMain {
     public static void main(String[] args) {
@@ -16,10 +13,15 @@ public class RunLitterSuperMarketConstructorMain {
         MerchandiseConstructor merchandiseGift2 = new MerchandiseConstructor
                 ("赠品-碗", "GIFT2", 200, 5, 0.5);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             MerchandiseConstructor merchandiseConstructor = new MerchandiseConstructor
                     ("商品" + (i + 1), "ID" + i, 10, (1 + Math.random()) * 200,
                             Math.random() * 200, merchandiseGift1);
+        }
+        for (int i = 5; i < 10; i++) {
+            MerchandiseConstructor merchandiseConstructor = new MerchandiseConstructor
+                    ("商品" + (i + 1), "ID" + i, 10, (1 + Math.random()) * 200,
+                            Math.random() * 200, merchandiseGift2);
         }
     }
 }
